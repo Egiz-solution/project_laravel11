8716fa645a2b776bbe3619bab12bb30407128605
arvindegiz <arvind@egizsolution.comm>
2024-06-14 18:03:50 +0530

    daliy database backup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -18,4 +19,11 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        /**
+         * Define the application's command schedule.
+         */
+        $schedule->command('db:backup')->daily();
+    })
+    ->create();
